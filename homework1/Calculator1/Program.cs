2221f9f84c31b2bd146b2ba7f349a52e7449ce74
hbc1444 <80057570+hbc1444@users.noreply.github.com>
2021-03-05 13:57:49 +0800

    Add files via upload

---
+++ homework1/Calculator1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请依次输入两个数：");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("请输入一个运算符(+-*/)：");
+            char q = Convert.ToChar(Console.ReadLine());
+            double z = 0;
+            switch (q)
+            {
+                case '+':
+                    z = x + y;
+                    Console.WriteLine(x + "+" + y + "=" + z);
+                    break;
+                case '-':
+                    z = x - y;
+                    Console.WriteLine(x + "-" + y + "=" + z);
+                    break;
+                case '*':
+                    z = x * y;
+                    Console.WriteLine(x + "*" + y + "=" + z);
+                    break;
+                case '%':
+                    z = x / y;
+                    Console.WriteLine(x + "/" + y + "=" + z);
+                    break;
+            }
+        }
+    }
+}
---
+++ homework1/WindowsFormscalculator/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormscalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedItem.ToString())
+            {
+                case "+":
+                    label1.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) + Convert.ToDouble(textBox2.Text));
+                    break;
+                case "-":
+                    label1.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) - Convert.ToDouble(textBox2.Text));
+                    break;
+                case "*":
+                    label1.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) * Convert.ToDouble(textBox2.Text));
+                    break;
+                case "/":
+                    label1.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) / Convert.ToDouble(textBox2.Text));
+                    break;
+            }
+            
+            
+           // label1.Text =Convert.ToString( Convert.ToDouble(textBox1.Text) + Convert.ToDouble(textBox2.Text));
+            
+        }
+    }
+}
